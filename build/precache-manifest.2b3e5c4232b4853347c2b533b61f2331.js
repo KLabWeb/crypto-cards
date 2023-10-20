99212b9e381e7f690f78d9d9e645fc4ee2a4d510
SmilingStallman <klabweb@tuta.io>
2023-10-20 05:34:35 -0400

    add production build

---
+++ build/precache-manifest.2b3e5c4232b4853347c2b533b61f2331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0381d482ab9d040e2ed1f92f116e852c",
+    "url": "/crypto-cards/index.html"
+  },
+  {
+    "revision": "17d32ac4e103c08d4393",
+    "url": "/crypto-cards/static/css/main.c6dc00c5.chunk.css"
+  },
+  {
+    "revision": "060a75340a2abebdb0b3",
+    "url": "/crypto-cards/static/js/2.2730378d.chunk.js"
+  },
+  {
+    "revision": "17d32ac4e103c08d4393",
+    "url": "/crypto-cards/static/js/main.97332448.chunk.js"
+  },
+  {
+    "revision": "3c0da16681a22b4851ea",
+    "url": "/crypto-cards/static/js/runtime-main.94edb3af.js"
+  }
+]);
